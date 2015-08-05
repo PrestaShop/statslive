e7b9a04c3d4b456528ddb7703f0659ef3a9b7492
Jérôme Nadaud <jerome.nadaud@prestashop.com>
2015-08-05 09:36:42 +0200

    // Version updated to v1.3.0

--- statslive.php
+++ statslive.php
@@ -35,7 +35,7 @@ class StatsLive extends Module
 	{
 		$this->name = 'statslive';
 		$this->tab = 'analytics_stats';
-		$this->version = '1.2.2';
+		$this->version = '1.3.0';
 		$this->author = 'PrestaShop';
 		$this->need_instance = 0;
 
