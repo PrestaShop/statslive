c87e41719a6c9b1705b5f27b1ebef4f1cb987578
Olivier Bonvalet <ob.kioob@daevel.fr>
2016-08-19 01:10:25 +0200

    getCustomersOnline(): fix SQL clauses about dates, to be able to use index

--- statslive.php
+++ statslive.php
@@ -72,7 +72,7 @@ class StatsLive extends Module
 					INNER JOIN `'._DB_PREFIX_.'customer` u ON u.id_customer = g.id_customer
 					WHERE cp.`time_end` IS NULL
 						'.Shop::addSqlRestriction(false, 'c').'
-						AND TIME_TO_SEC(TIMEDIFF(\''.pSQL(date('Y-m-d H:i:00', time())).'\', cp.`time_start`)) < 900
+						AND `cp.time_start` > NOW() - interval 900 second
 					'.($maintenance_ips ? 'AND c.ip_address NOT IN ('.preg_replace('/[^,0-9]/', '', $maintenance_ips).')' : '').'
 					GROUP BY u.id_customer
 					ORDER BY u.firstname, u.lastname';
@@ -83,7 +83,7 @@ class StatsLive extends Module
 					FROM `'._DB_PREFIX_.'connections` c
 					INNER JOIN `'._DB_PREFIX_.'guest` g ON c.id_guest = g.id_guest
 					INNER JOIN `'._DB_PREFIX_.'customer` u ON u.id_customer = g.id_customer
-					WHERE TIME_TO_SEC(TIMEDIFF(\''.pSQL(date('Y-m-d H:i:00', time())).'\', c.`date_add`)) < 900
+					WHERE `c.date_add` > NOW() - interval 900 second
 						'.Shop::addSqlRestriction(false, 'c').'
 					'.($maintenance_ips ? 'AND c.ip_address NOT IN ('.preg_replace('/[^,0-9]/', '', $maintenance_ips).')' : '').'
 					GROUP BY u.id_customer
