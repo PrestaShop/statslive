12a96dc901ef76944b9b9f9e2329d6fe5ad3394c
Maxime Biloé <maxime.biloe@gmail.com>
2017-01-12 18:07:53 +0100

    // Version updated to v1.3.1

--- statslive.php
+++ statslive.php
@@ -35,7 +35,7 @@ class StatsLive extends Module
 	{
 		$this->name = 'statslive';
 		$this->tab = 'analytics_stats';
-		$this->version = '1.3.0';
+		$this->version = '1.3.1';
 		$this->author = 'PrestaShop';
 		$this->need_instance = 0;
 
