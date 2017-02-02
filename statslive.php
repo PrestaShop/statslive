70d7d3eb8c35e8ea1f6c65488aa3507da6cf82d5
Alex Sampaio <alex.sampaio1991@gmail.com>
2017-02-02 09:18:06 +0100

    // Version updated to v2.0.2

--- statslive.php
+++ statslive.php
@@ -36,7 +36,7 @@ class statslive extends Module
     {
         $this->name = 'statslive';
         $this->tab = 'analytics_stats';
-        $this->version = '2.0.1';
+        $this->version = '2.0.2';
         $this->author = 'PrestaShop';
         $this->need_instance = 0;
 
